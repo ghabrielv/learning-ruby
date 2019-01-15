ad509739a535c6cc525ff88f4fd4ed425110a07b
Ghabriel Villarreal <ghabrielv@gmail.com>
2019-01-15 12:18:32 -0400

    Using inputs

--- square.rb
+++ square.rb
@@ -1,3 +1,4 @@
-side = 5
+print "Enter the longitude of the side of the square: "
+side = gets.chomp.to_i
 puts "The perimeter of a square of side #{side} is #{side * 4}"
 puts "The area of a square of side #{side} is #{side * side}"
