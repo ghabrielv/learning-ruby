1e52fab6500e518ecd07fc6bc2ae8a20e1480f1c
Ghabriel Villarreal <ghabrielv@gmail.com>
2019-01-15 11:53:29 -0400

    Operations with numbers

---
+++ numbers.rb
@@ -0,0 +1,9 @@
+puts 1 + 2
+puts 3 * 4 + 5
+puts 8 / 2
+puts "1" + '2'
+puts 3 * (4 + 5) 
+puts "2 + 3 is 5"
+puts "2 + 3 is " + (2 + 3).to_s
+puts '2 + 3 is #{2 + 3}'
+puts "2 + 3 is #{2 + 3} using interpolation"
